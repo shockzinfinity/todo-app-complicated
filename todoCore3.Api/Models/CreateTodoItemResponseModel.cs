6673d64382dde51d3f94c54aefced27fe8438f4d
Jun Yu <shockz@shockz.io>
2020-09-29 00:38:40 +0900

    Add CommandHandler, QueryHandler

---
+++ todoCore3.Api/Models/CreateTodoItemResponseModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace todoCore3.Api.Models
+{
+  public class CreateTodoItemResponseModel
+  {
+  }
+}
