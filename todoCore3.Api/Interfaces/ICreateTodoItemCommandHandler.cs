ee03a33762c30ff015214ac3e92b2f49d74eed35
Jun Yu <shockz@shockz.io>
2020-09-29 00:40:09 +0900

    interfaces

--- todoCore3.Api/Interfaces/ICreateTodoItemCommandHandler.cs
+++ todoCore3.Api/Interfaces/ICreateTodoItemCommandHandler.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using todoCore3.Api.Models;
 
 namespace todoCore3.Api.Interfaces
 {
-  public class ICreateTodoItemCommandHandler
+  public interface ICreateTodoItemCommandHandler
   {
+    CreateTodoItemResponseModel CreateTodoItem(CreateTodoItemRequestModel requestModel);
   }
 }
--- todoCore3.Api/Interfaces/IGetTodoItemByIdQueryHandler.cs
+++ todoCore3.Api/Interfaces/IGetTodoItemByIdQueryHandler.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using todoCore3.Api.Models;
 
 namespace todoCore3.Api.Interfaces
 {
-  public class IGetTodoItemByIdQueryHandler
+  public interface IGetTodoItemByIdQueryHandler
   {
+    GetTodoItemByIdResponseModel GetTodoItemById(GetTodoItemByIdRequestModel requestModel);
   }
 }
