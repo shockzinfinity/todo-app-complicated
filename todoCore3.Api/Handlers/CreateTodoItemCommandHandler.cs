04eba295b94b582b6c5f094913803865ea467d81
Jun Yu <shockz@shockz.io>
2020-09-29 00:42:37 +0900

    command handler, query handler

---
+++ todoCore3.Api/Handlers/CreateTodoItemCommandHandler.cs
@@ -0,0 +1,14 @@
+using System;
+using todoCore3.Api.Interfaces;
+using todoCore3.Api.Models;
+
+namespace todoCore3.Api.Handlers
+{
+  public class CreateTodoItemCommandHandler : ICreateTodoItemCommandHandler
+  {
+    public CreateTodoItemResponseModel CreateTodoItem(CreateTodoItemRequestModel requestModel)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
---
+++ todoCore3.Api/Handlers/GetTodoItemByIdQueryHandler.cs
@@ -0,0 +1,14 @@
+using System;
+using todoCore3.Api.Interfaces;
+using todoCore3.Api.Models;
+
+namespace todoCore3.Api.Handlers
+{
+  public class GetTodoItemByIdQueryHandler : IGetTodoItemByIdQueryHandler
+  {
+    public GetTodoItemByIdResponseModel GetTodoItemById(GetTodoItemByIdRequestModel requestModel)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
