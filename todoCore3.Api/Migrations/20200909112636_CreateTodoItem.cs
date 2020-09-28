3ec6527ac1a1578db78aa01f985f88f6ee4be0ed
Jun Yu <shockz@shockz.io>
2020-09-28 23:39:36 +0900

    code maid

--- todoCore3.Api/Migrations/20200909112636_CreateTodoItem.cs
+++ todoCore3.Api/Migrations/20200909112636_CreateTodoItem.cs
@@ -1,30 +1,30 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace todoCore3.Api.Migrations
 {
-	public partial class CreateTodoItem : Migration
-	{
-		protected override void Up(MigrationBuilder migrationBuilder)
-		{
-			migrationBuilder.CreateTable(
-				name: "TodoItems",
-				columns: table => new
-				{
-					Id = table.Column<long>(nullable: false)
-						.Annotation("SqlServer:Identity", "1, 1"),
-					Name = table.Column<string>(nullable: true),
-					IsCompleted = table.Column<bool>(nullable: false)
-				},
-				constraints: table =>
-				{
-					table.PrimaryKey("PK_TodoItems", x => x.Id);
-				});
-		}
+  public partial class CreateTodoItem : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.CreateTable(
+        name: "TodoItems",
+        columns: table => new
+        {
+          Id = table.Column<long>(nullable: false)
+            .Annotation("SqlServer:Identity", "1, 1"),
+          Name = table.Column<string>(nullable: true),
+          IsCompleted = table.Column<bool>(nullable: false)
+        },
+        constraints: table =>
+        {
+          table.PrimaryKey("PK_TodoItems", x => x.Id);
+        });
+    }
 
-		protected override void Down(MigrationBuilder migrationBuilder)
-		{
-			migrationBuilder.DropTable(
-				name: "TodoItems");
-		}
-	}
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropTable(
+        name: "TodoItems");
+    }
+  }
 }
--- todoCore3.Api/Migrations/20200912110359_AddTimeStamp.cs
+++ todoCore3.Api/Migrations/20200912110359_AddTimeStamp.cs
@@ -1,24 +1,23 @@
-﻿using System;
 using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace todoCore3.Api.Migrations
 {
-    public partial class AddTimeStamp : Migration
+  public partial class AddTimeStamp : Migration
+  {
+    protected override void Up(MigrationBuilder migrationBuilder)
     {
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.AddColumn<byte[]>(
-                name: "RowVersion",
-                table: "TodoItems",
-                rowVersion: true,
-                nullable: true);
-        }
-
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-            migrationBuilder.DropColumn(
-                name: "RowVersion",
-                table: "TodoItems");
-        }
+      migrationBuilder.AddColumn<byte[]>(
+          name: "RowVersion",
+          table: "TodoItems",
+          rowVersion: true,
+          nullable: true);
     }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+      migrationBuilder.DropColumn(
+          name: "RowVersion",
+          table: "TodoItems");
+    }
+  }
 }
