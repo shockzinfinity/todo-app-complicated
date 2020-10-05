d07bac71c6e8a394aa6899a21a50f2d454fac83d
Jun Yu <shockz@me.com>
2020-10-05 17:11:55 +0900

    작업중

--- todoCore3.Api/Models/FlowDTO.cs
+++ todoCore3.Api/Models/FlowDTO.cs
@@ -1,17 +1,16 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace todoCore3.Api.Models
 {
-  public class CategoryDTO
+  public class FlowDTO
   {
     public long Id { get; set; }
     public string Name { get; set; }
-    public string BgColor { get; set; }
-    public int UserId { get; set; }
+    public long Pos { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
+    public long CategoryId { get; set; }
   }
 }
