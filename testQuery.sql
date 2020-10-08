fff2a56952ba3c463f9a9cb0cf4b68a3008f7936
Jun Yu <shockz@me.com>
2020-10-08 19:59:00 +0900

    수정중

--- testQuery.sql
+++ testQuery.sql
@@ -1,8 +1,13 @@
-SELECT * FROM [dbo].[__EFMigrationsHistory]
-SELECT * FROM [dbo].[Users]
-SELECT * FROM [dbo].[Categories]
-SELECT * FROM [dbo].[TodoItems]
-SELECT * FROM [dbo].[Flows]
+SELECT *
+FROM [dbo].[__EFMigrationsHistory]
+SELECT *
+FROM [dbo].[Users]
+SELECT *
+FROM [dbo].[Categories]
+SELECT *
+FROM [dbo].[TodoItems]
+SELECT *
+FROM [dbo].[Flows]
 
 -- UPDATE [dbo].[Categories]
 -- SET [BgColor] = 'rgb(191, 0, 121)'
@@ -20,14 +25,19 @@ SELECT * FROM [dbo].[Flows]
 -- INSERT INTO [dbo].[Flows] ([Name], [Pos], [CreatedAt], [UpdatedAt], [CategoryId])
 -- VALUES (N'완료', 262144, GETDATE(), GETDATE(), 36)
 
-SELECT * FROM [dbo].[TodoItems]
-SELECT * FROM [dbo].[Flows]
+SELECT *
+FROM [dbo].[TodoItems]
+SELECT *
+FROM [dbo].[Flows]
 
 65536
 131072
 196608
 
-SELECT * FROM [dbo].[TodoItems]
+SELECT *
+FROM [dbo].[TodoItems]
+WHERE [FlowId] = 7
+
 
 -- UPDATE [dbo].[TodoItems]
 -- SET [Pos] = 65536 * 7
