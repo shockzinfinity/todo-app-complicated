1afa8d8da2744c1307390394159f7d755af2dff2
Jun Yu <shockz@me.com>
2020-10-08 18:37:29 +0900

    TodoItem > Description 추가 및 마이그레이션

---
+++ todoCore3.Api/DirtyProperties.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+
+namespace todoCore3.Api
+{
+  public static class DirtyProperties
+  {
+    public static T ToUpdatedObject<T>(T entityObject)
+    {
+      return UpdateObject(entityObject, GetDirtyProperties());
+    }
+
+    private static Dictionary<string, object> GetDirtyProperties()
+    {
+      //Inspects the JSON payload for properties explicitly set.
+      return JsonConvert.DeserializeObject<Dictionary<string, object>>(new StreamReader(HttpContext.Current.Request.InputStream).ReadToEnd());
+    }
+
+    private static T UpdateObject<T>(T entityObject, Dictionary<string, object> properties)
+    {
+
+      //Loop through each changed properties and update the entity object with new values
+      foreach (var prop in properties)
+      {
+        var updateProperty = entityObject.GetType().GetProperty(prop.Key);// Try and get property
+
+        if (updateProperty != null)
+        {
+          SetValue(updateProperty, entityObject, prop.Value);
+        }
+      }
+
+      return entityObject;
+    }
+
+    private static void SetValue(PropertyInfo property, object entity, object newValue)
+    {
+      //This method is used to convert binding model properties to entity properties and set the new value
+      Type t = Nullable.GetUnderlyingType(property.PropertyType) ?? property.PropertyType;
+      object safeVal = (newValue == null) ? null : Convert.ChangeType(newValue, t);
+
+      property.SetValue(entity, safeVal);
+    }
+  }
+}
--- todoCore3.Api/Models/TodoItemDTO.cs
+++ todoCore3.Api/Models/TodoItemDTO.cs
@@ -8,6 +8,7 @@ namespace todoCore3.Api.Models
     public long FlowId { get; set; }
     public long Pos { get; set; }
     public string Name { get; set; }
+    public string Description { get; set; }
     public bool IsComplete { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
