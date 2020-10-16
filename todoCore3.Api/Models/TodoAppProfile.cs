fde9a7bd37571e4ebed8a98c052da708bd29bc54
Jun Yu <shockz@me.com>
2020-10-16 18:37:54 +0900

    data context 수정

--- todoCore3.Api/Models/TodoAppProfile.cs
+++ todoCore3.Api/Models/TodoAppProfile.cs
@@ -1,4 +1,6 @@
 using AutoMapper;
+using todoCore3.Api.Models.Auth;
+using todoCore3.Api.Models.Auth.Entities;
 
 namespace todoCore3.Api.Models
 {
@@ -12,6 +14,26 @@ namespace todoCore3.Api.Models
       CreateMap<Category, CategoryDto>();
       CreateMap<CategoryDto, Category>();
       CreateMap<Category, CategoryWithItems>();
+
+      #region Accounts profiles
+
+      CreateMap<Account, AccountResponse>();
+      CreateMap<Account, AuthenticateResponse>();
+      CreateMap<RegisterRequest, Account>();
+      CreateMap<CreateAccountRequest, Account>();
+      CreateMap<UpdateAccountRequest, Account>()
+        .ForAllMembers(x => x.Condition(
+          (src, dest, prop) =>
+          {
+            if (prop == null) return false;
+            if (prop.GetType() == typeof(string) && string.IsNullOrEmpty((string)prop)) return false;
+
+            if (x.DestinationMember.Name == "Role" && src.Role == null) return false;
+
+            return true;
+          }));
+
+      #endregion
     }
   }
 }
